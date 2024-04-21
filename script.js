99b25c9b41a009be25453e261607e7996ecfec10
Ishan Jayasinghe <ishanrashithajayasinghe@gmail.com>
2024-04-21 06:49:47 +0530

    Copy to clipboard functionality added. JS script moved to seperate JS file. Some disign changes added. Favicon added.

---
+++ script.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    // Get a new quote
+    function getQuote() {
+        $.ajax({
+            url: 'https://api.quotable.io/random',
+            success: function(data) {
+                $('#text').html('<span id="text-before" class="color-change"><i class="fa-solid fa-quote-left"></i></span>' +" "+ data.content);
+                $('#author').text("- "+data.author);
+                $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?text=' + encodeURIComponent('"' + data.content + '" ' + data.author));
+
+                const color = 'rgb(' + Math.floor(Math.random() * 128) + ',' + Math.floor(Math.random() * 128) + ',' + Math.floor(Math.random() * 128) + ')';
+                $('.color-change').css('color', color);
+                $('.bg-color-change').css('background-color', color);
+            }
+        });
+    }
+
+    // Get a new quote when the page loads
+    getQuote();
+
+    // Get a new quote when the button is clicked
+    $('#new-quote').on('click', getQuote);
+
+    // Copy to clipboard
+    $('#copy-clipboard').on('click', function() {
+        const text = $('#text').text() + " " + $('#author').text();
+        navigator.clipboard.writeText(text).then(function() {
+            $('#copy-clipboard').text('Copied!');
+            setTimeout(function() {
+                $('#copy-clipboard').html('<i class="fa fa-clipboard" aria-hidden="true"></i>');
+            }, 1000);
+        }, function(err) {
+            console.error('Failed to copy!', err);
+        });
+    });
+});
